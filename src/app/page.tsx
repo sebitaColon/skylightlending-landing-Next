4ddf463b6383df55421d506050c9898f70112746
SebitaColon <151690133+sebitaColon@users.noreply.github.com>
2024-08-28 23:09:16 -0300

    add layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,19 @@
 "use client"
-import Footer from "@/components/footer";
-import Nav from "@/components/nav";
 import Head from "@/components/head";
 import AboutUs from "@/components/AboutUs";
 import Banner from "@/components/Banner";
 import Portales from "@/components/Portales";
 import Information from "@/components/Information";
+import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-    <>
-      <Nav></Nav>
+    <Layout>
       <Head />
       <AboutUs />
       <Banner />
       <Portales />
       <Information />
-      <Footer></Footer>
-    </>
+    </Layout>
   );
 }
