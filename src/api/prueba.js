8c468979c0b87c17d72a24e53941f720699202f5
Alejandro98123 <176923232+Alejandro98123@users.noreply.github.com>
2024-09-02 12:16:04 -0300

    config database

---
+++ src/api/prueba.js
@@ -0,0 +1,10 @@
+/*import {pool} from "../config/conexion"
+
+export default async function prueba(req, res) {
+   const result = await pool.query("SELECT * FROM usuarios")
+    
+   console.log(result)
+    return res.status(200).json({result})
+}*/
+
+console.log("estoy aca")
