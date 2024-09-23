fb4107c4b32a975a2b76010db710619555eaae5d
Alejandro98123 <176923232+Alejandro98123@users.noreply.github.com>
2024-09-23 11:53:12 -0300

    add validation with React Hook Form

--- src/app/Installer/page.tsx
+++ src/app/Installer/page.tsx
@@ -4,56 +4,26 @@ import Layout from "../../components/Layout";
 import { Input, Button } from "@nextui-org/react";
 import BackgrounImg from "../../assets/FondoFormInstaller.jpg";
 import Image from "next/image";
-import { useState } from "react";
-import schema from "@/validation/installerSchema";
+import { useForm } from "react-hook-form";
 import toast, { Toaster } from "react-hot-toast";
 
 function Installer() {
-  // Estado para manejar el color del borde del input y los errores
-  const [alerts, setAlerts] = useState(true);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+    trigger,
+  } = useForm({
+    mode: "onChange",
+  });
 
-  // Valores iniciales del formulario
-  const initialFormValues = {
-    name: "",
-    lastName: "",
-    companyName: "",
-    contactNumber: "",
-    email: "",
-    website: "",
-    references: "",
+  const onSubmit = (data: any) => {
+    toast.success("Formulario enviado correctamente.");
+    console.log(data);
+    reset();
   };
 
-  const [formValues, setFormValues] = useState(initialFormValues);
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormValues((prevValues) => ({ ...prevValues, [name]: value }));
-  };
-
-  // Función para vaciar los campos del formulario
-  const handleReset = () => {
-    setFormValues(initialFormValues);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      // Validar los valores del formulario
-      await schema.validate(formValues, { abortEarly: false });
-      // Si la validación pasa
-      toast.success("Well done! All errors are fixed.");
-      handleReset();
-      setAlerts(true);
-
-      // Aquí puedes manejar el envío de datos, si es necesario
-    } catch (e) {
-      toast.error(
-        "There error on this page. Please correct it before moving on.."
-      );
-      setAlerts(false);
-    }
-  };
-
-  // Contar los errores al renderizar
   return (
     <Layout>
       <div className="w-full h-auto flex flex-col items-center justify-center relative">
@@ -63,145 +33,165 @@ function Installer() {
           className="absolute object-cover w-full h-full -z-10"
         />
         <Toaster position="bottom-center" />
-        <div
-          className={`w-auto h-auto p-0.5 mt-40 mb-20 rounded-3xl transition-all duration-500 ease-in-out`}
-        >
-          <form onSubmit={handleSubmit} className="bg-white p-5 rounded-3xl ">
+        <div className="w-auto h-auto p-0.5 mt-40 mb-20 rounded-3xl transition-all duration-500 ease-in-out">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="bg-white p-5 rounded-3xl lg:min-w-[800px] "
+          >
             <h1 className="font-InterBold text-center text-black text-md sm:text-3xl pb-10 mb-5 border-b-1">
               <span className="text-blue-600 pr-1">Skylight Lending New</span>
               Dealer Application
             </h1>
-            <div className="w-full grid sm:grid-cols-2 sm:grid-rows-4 gap-5 bg-white">
-              <div className="h-auto flex items-end">
+            <div className="w-full grid sm:grid-cols-2  sm:grid-rows-4 gap-x-5  bg-white">
+              <div className="h-auto flex flex-col">
                 <Input
-                  name="name"
+                  {...register("name", {
+                    required: "El nombre es obligatorio",
+                    minLength: {
+                      value: 2,
+                      message: "El nombre debe tener al menos 2 caracteres",
+                    },
+                  })}
                   radius="sm"
                   variant="underlined"
-                  color={alerts ? "success" : "danger"}
                   size="lg"
-                  value={formValues["name"]}
-                  onChange={handleChange}
-                  isRequired
-                  type="text"
                   label="Your Name "
-                  labelPlacement="outside"
                   placeholder="First Name"
                   className="w-full h-auto px-2 text-black"
-                  isInvalid={!alerts}
+                  onBlur={() => trigger("name")} // Valida cuando el campo pierde el foco
                 />
-                <Input
-                  name="lastName"
-                  radius="sm"
-                  variant="underlined"
-                  color={alerts ? "success" : "danger"}
-                  size="lg"
-                  value={formValues["lastName"]}
-                  onChange={handleChange}
-                  isRequired
-                  type="text"
-                  label="Last name "
-                  labelPlacement="outside"
-                  placeholder="First Name"
-                  className="w-full h-auto px-2 text-black"
-                  isInvalid={!alerts}
-                />
+                -
+                {errors.name && (
+                  <p className="text-red-500 h-auto max-w-[350px] text-sm mt-1 px-2">
+                    {errors.name.message}
+                  </p>
+                )}
               </div>
-              <div className="w-full">
+
+              <div className="h-auto flex flex-col">
                 <Input
-                  name="companyName"
+                  {...register("lastName", {
+                    required: "El apellido es obligatorio",
+                    minLength: {
+                      value: 2,
+                      message: "El apellido debe tener al menos 2 caracteres",
+                    },
+                  })}
                   radius="sm"
                   variant="underlined"
-                  color={alerts ? "success" : "danger"}
                   size="lg"
-                  onChange={handleChange}
-                  value={formValues["companyName"]}
-                  isRequired
-                  type="text"
+                  label="Last Name "
+                  placeholder="Last Name"
+                  className="w-full h-auto px-2 text-black"
+                  onBlur={() => trigger("lastName")}
+                />
+                {errors.lastName && (
+                  <p className="text-red-500 h-auto  max-w-[350px] text-sm mt-1 px-2">
+                    {errors.lastName.message}
+                  </p>
+                )}
+              </div>
+
+              <div className="w-full h-auto flex flex-col">
+                <Input
+                  {...register("companyName", {
+                    required: "El nombre de la empresa es obligatorio",
+                    minLength: {
+                      value: 4,
+                      message:
+                        "El nombre de la empresa debe tener al menos 4 caracteres",
+                    },
+                  })}
+                  radius="sm"
+                  variant="underlined"
+                  size="lg"
                   label="Company Name "
                   placeholder="SkyLight"
-                  labelPlacement="outside"
                   className="w-full h-auto px-2"
-                  isInvalid={!alerts}
+                  onBlur={() => trigger("companyName")}
                 />
+                {errors.companyName && (
+                  <p className="text-red-500 h-auto max-w-[350px] text-sm mt-1 px-2">
+                    {errors.companyName.message}
+                  </p>
+                )}
               </div>
-              <div className="w-full">
+
+              <div className="w-full h-auto flex flex-col">
                 <Input
-                  name="contactNumber"
+                  {...register("contactNumber", {
+                    required: "El número de contacto es obligatorio",
+                    minLength: {
+                      value: 10,
+                      message:
+                        "El número de contacto debe tener al menos 10 dígitos",
+                    },
+                  })}
                   radius="sm"
                   variant="underlined"
-                  color={alerts ? "success" : "danger"}
                   size="lg"
-                  onChange={handleChange}
-                  value={formValues["contactNumber"]}
-                  isRequired
-                  type="number"
                   label="Contact Number "
                   placeholder="(000) 000-0000"
-                  labelPlacement="outside"
                   className="w-full h-auto px-2"
-                  isInvalid={!alerts}
+                  onBlur={() => trigger("contactNumber")}
                 />
+                {errors.contactNumber && (
+                  <p className="text-red-500 h-auto max-w-[350px] text-sm mt-1 px-2">
+                    {errors.contactNumber.message}
+                  </p>
+                )}
               </div>
-              <div className="w-full">
+
+              <div className="w-full h-auto flex flex-col">
                 <Input
-                  name="email"
+                  {...register("email", {
+                    required: "El correo es obligatorio",
+                    pattern: {
+                      value: /^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/,
+                      message: "Ingrese un correo válido",
+                    },
+                  })}
                   radius="sm"
                   variant="underlined"
-                  color={alerts ? "success" : "danger"}
-                  onChange={handleChange}
-                  value={formValues["email"]}
                   size="lg"
-                  isRequired
-                  type="email"
                   label="E-mail "
                   placeholder="ex: myname@example.com"
-                  labelPlacement="outside"
                   className="w-full h-auto px-2"
-                  isInvalid={!alerts}
+                  onBlur={() => trigger("email")}
                 />
+                {errors.email && (
+                  <p className="text-red-500 max-w-[350px] text-sm mt-1 px-2">
+                    {errors.email.message}
+                  </p>
+                )}
               </div>
-              <div className="w-full">
+
+              <div className="w-full h-auto flex flex-col">
                 <Input
-                  name="website"
+                  {...register("website")}
                   radius="sm"
                   variant="underlined"
-                  color="default"
                   size="lg"
-                  onChange={handleChange}
-                  value={formValues["website"]}
-                  startContent={
-                    <div className="pointer-events-none flex items-center">
-                      <span className="text-default-400 text-small">
-                        https://
-                      </span>
-                    </div>
-                  }
                   type="url"
                   label="Company Website "
                   placeholder="www.example.com"
-                  labelPlacement="outside"
-                  className="w-full h-auto px-2 sm:mb-4 lg:mb-0"
-                />
-              </div>
-              <div className="h-0"></div>
-              <div className="w-full">
-                <Input
-                  name="references"
-                  radius="sm"
-                  variant="underlined"
-                  color="default"
-                  size="lg"
-                  onChange={handleChange}
-                  value={formValues["references"]}
-                  type="text"
-                  label="References (i.e. Account Manager, Contacts, etc.) "
-                  placeholder=" "
-                  labelPlacement="outside"
                   className="w-full h-auto px-2"
                 />
               </div>
-              <div className=""></div>
+
+              <div className="w-full h-auto flex flex-col">
+                <Input
+                  {...register("references")}
+                  radius="sm"
+                  variant="underlined"
+                  size="lg"
+                  type="text"
+                  label="References (i.e. Account Manager, Contacts, etc.) "
+                  className="w-full h-auto px-2"
+                />
+              </div>
             </div>
+
             <div className="mt-10 flex border-t-1 p-5 w-full items-end justify-end">
               <Button
                 className="mr-2"
